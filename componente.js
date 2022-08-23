0e6a8b32c6c6b6deb58ddec25cdddc2b1a14d2dc
Débora <d.saioron@hotmail.com>
2022-08-23 19:59:48 -0300

    testando funções

--- componente.js
+++ componente.js
@@ -1,5 +1,6 @@
 const todosBotoes = document.querySelectorAll(".opcoes");
 const submit = document.querySelector(".enviar");
+const error = document.getElementById("error-msg");
 const resultado = document.querySelector(".classificacao_escolhida");
 const box1 = document.getElementById("boxum");
 const box2 = document.getElementById("boxdois");
@@ -16,11 +17,32 @@ todosBotoes.forEach(btn => {
 
 
 submit.addEventListener("click", () => {
-    
+    if (selected === "") {
+        addBotaoErros()
+        setTimeout(() => {
+            removeBotaoErros()
+        }, 3000)
+        return
+    }
+
     resultado.textContent = `Você selecionou ${selected} de 5`
     box1.classList.add("d-none");
     box2.classList.remove("d-none");
 })
+
+function addBotaoErros() {
+    error.classList.remove("d-none")
+    todosBotoes.forEach(btn => {
+        btn.classList.add("error")
+    })
+}
+
+function removeBotaoErros() {
+    error.classList.add("d-none")
+    todosBotoes.forEach(btn => {
+        btn.classList.remove("error")
+    })
+}
 
 function removeSelectedClasses() {
     botoes.forEach(btn => {
