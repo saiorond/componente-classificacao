070a13fdc3be1d74b842ac989182980471e055e2
Débora <d.saioron@hotmail.com>
2022-08-23 22:02:43 -0300

    finalizando

--- componente.js
+++ componente.js
@@ -1,5 +1,6 @@
 const todosBotoes = document.querySelectorAll(".opcoes");
 const submit = document.querySelector(".enviar");
+const submit2 = document.querySelector(".classificar_novamente");
 const box1 = document.getElementById("boxum");
 const box2 = document.getElementById("boxdois");
 const escolha = document.getElementById("escolha");
@@ -9,6 +10,11 @@ submit.addEventListener("click", () => {
   box2.classList.remove("hidden")
   box1.style.display = "none"
 });
+
+submit2.addEventListener("click", () => {
+    box2.classList.add("hidden")
+    box1.style.display = "block"
+  });
 
 todosBotoes.forEach((opcao) => {
     opcao.addEventListener("click", () => {
