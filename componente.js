e8ff96fdd33bf913270edc5ae978308ee370edb1
Débora <d.saioron@hotmail.com>
2022-08-23 21:30:53 -0300

    finalizando js

--- componente.js
+++ componente.js
@@ -1,51 +1,17 @@
 const todosBotoes = document.querySelectorAll(".opcoes");
 const submit = document.querySelector(".enviar");
-const error = document.getElementById("error-msg");
-const resultado = document.querySelector(".classificacao_escolhida");
 const box1 = document.getElementById("boxum");
 const box2 = document.getElementById("boxdois");
-
-let selected = "";
-
-todosBotoes.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        removeSelectedClasses()
-        selected = e.target.textContent
-        e.target.classList.add("selected")
-    })
-})
+const escolha = document.getElementById("escolha");
 
 
 submit.addEventListener("click", () => {
-    if (selected === "") {
-        addBotaoErros()
-        setTimeout(() => {
-            removeBotaoErros()
-        }, 3000)
-        return
-    }
+  box2.classList.remove("hidden")
+  box1.style.display = "none"
+});
 
-    resultado.textContent = `Você selecionou ${selected} de 5`
-    box1.classList.add("d-none");
-    box2.classList.remove("d-none");
+todosBotoes.forEach((opcao) => {
+    opcao.addEventListener("click", () => {
+        escolha.innerHTML = opcao.innerHTML
+    })
 })
-
-function addBotaoErros() {
-    error.classList.remove("d-none")
-    todosBotoes.forEach(btn => {
-        btn.classList.add("error")
-    })
-}
-
-function removeBotaoErros() {
-    error.classList.add("d-none")
-    todosBotoes.forEach(btn => {
-        btn.classList.remove("error")
-    })
-}
-
-function removeSelectedClasses() {
-    botoes.forEach(btn => {
-        btn.classList.remove("selected")
-    })
-}
